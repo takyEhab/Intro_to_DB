96521f5da13a604432c113c77d6fa8ebcab0a4bb
takyEhab <tkyehab@gmail.com>
2025-02-05 13:17:16 +0200

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,7 @@
-ANALYZE SELECT *
-FROM books
+-- ANALYZE SELECT *
+-- FROM books
+
+SELECT column_name, data_type 
+FROM INFORMATION_SCHEMA.COLUMNS 
+where table_name = 'columns'
+  and table_schema = 'information_schema';
