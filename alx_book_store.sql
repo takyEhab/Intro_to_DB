266f492acb37426c54c18562b0510ef734136c14
takyEhab <tkyehab@gmail.com>
2025-02-05 12:54:22 +0200

    FINISH BOOK STORE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -103,14 +103,14 @@ DROP TABLE IF EXISTS `Order_Details`;
 CREATE TABLE `Order_Details` (
   `orderdetailid` int NOT NULL,
   `order_id` int NOT NULL,
-  `book_id` int NOT NULL,
+  book_id INT NOT NULL,
   `order_date` date DEFAULT NULL,
-  `quantity` double DEFAULT NULL,
+  quantity DOUBLE DEFAULT NULL,
   PRIMARY KEY (`orderdetailid`),
   KEY `order_id` (`order_id`),
   KEY `book_id` (`book_id`),
-  CONSTRAINT `Order_Details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `Orders` (`order_id`),
-  CONSTRAINT `Order_Details_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `Books` (`book_id`)
+  CONSTRAINT `Order_Details_ibfk_1` FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  CONSTRAINT `Order_Details_ibfk_2` FOREIGN KEY (book_id) REFERENCES Books(book_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
